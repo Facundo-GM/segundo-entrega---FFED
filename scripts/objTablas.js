3ee1e0445ab1e8a263039db6acfda2bdbe2a31c3
Facundo Moreno <morenofacundoFGM@gmail.com>
2023-11-01 02:08:05 -0300

    finalizacion de tarea editar producto

--- scripts/objTablas.js
+++ scripts/objTablas.js
@@ -3,6 +3,7 @@ const pr = JSON.parse(localStorage.getItem("-productos")) || [];
 
 const tabla = document.getElementById("prtl");
 
+
 if(pr.length > 0){
     pr.forEach(product => {
         tabla.innerHTML += 
@@ -12,10 +13,14 @@ if(pr.length > 0){
             <td>${product.nombre}</td>
             <td>${product.valor}$</td>
             <td>
-                <a href="../html/editarPR.html?=${product.id}"><button type="button" class="btn btn-outline-warning">Editar</button></a>
-                <button type="button" class="btn btn-danger btnElim" data-ide="${product.id}">
-                    Eliminar
-                </button>
+                <div class="d-flex gap-3 opciones">
+                    <a href="../html/editarPR.html?=${product.id}"><button type="button" class="btn btn-outline-warning">Editar</button></a>
+                    <button type="button" class="btn btn-danger btnElim" data-ide="${product.id}">
+                        Eliminar
+                    </button>
+                    <i class="bi bi-star-fill destacar" data-ident="${product.id}"></i>
+
+                </div>
             </td>
         </tr>
         `
@@ -57,5 +62,54 @@ btnElim.forEach((tag)=>{
                 
             }
         });
+    });
+});
+
+const btnDest = document.querySelectorAll('.destacar');
+const imgDest = JSON.parse(localStorage.getItem("-prDest")) || [];
+
+btnDest.forEach((tag)=>{
+
+    
+    tag.addEventListener('click', (event)=> {
+        const dataIdent = event.target.getAttribute('data-ident');
+
+        let destacado = {
+            id:dataIdent,
+            image:pr[dataIdent-1].imagenes[0]
+        };
+
+        if(imgDest.length != 0){
+            Swal.fire({
+                title: 'ESPERA',
+                text: "¿Si destacas este producto se reemplazara el anterior?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, Quiero borrarlo !'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    'Destacado!',
+                    'Sigamos trabajando.',
+                    'success'
+                 );
+
+                 localStorage.setItem("-prDest",JSON.stringify(destacado))
+                }
+            });
+            
+        }
+        else{
+            imgDest.push(destacado)
+            localStorage.setItem("-prDest",JSON.stringify(destacado))
+            Swal.fire(
+                'Bien!',
+                `Se destaco el producto de ID: ${dataIdent}!`,
+                'success'
+            );
+        }
+        
     });
 });
